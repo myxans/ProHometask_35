6d67ae43bfeca7c357b93f28aa267fa88d5c450f
myxans <mushenko1992@gmail.com>
2023-04-17 10:19:05 +0300

    remove

--- index.js
+++ index.js
@@ -1,44 +1,60 @@
-class Human {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-
-  printInfo() {
-    console.log(`Name: ${this.name}, Age: ${this.age}`);
-  }
-};
-
-class Car {
-  constructor(brand, model, year, licensePlate, owner) {
-    this.brand = brand;
-    this.model = model;
-    this.year = year;
-    this.licensePlate = licensePlate;
-    if (owner.age >= 18) {
-      this.owner = owner;
-    } else {
-      console.log("The owner must be over 18 years old!");
+// Створюємо сутність людина
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
     }
-  }
 
-  printInfo() {
-    console.log(
-      `Brand: ${this.brand}, Model: ${this.model}, Year: ${this.year}, License Plate: ${this.licensePlate}`
-    );
-    console.log("Owner Information:");
-    this.owner.printInfo();
-  }
+    displayInfo() {
+    console.log (`Ім'я: ${this.name}, Вік: ${this.age}`)
+}
 };
 
-let human1 = new Human("John Doe", 25);
-let human2 = new Human("Jane Smith", 32);
-let human3 = new Human("Bob Johnson", 19);
+// Створюємо сутність автомобіль
+class Car {
+    constructor (brand, model, year, licensePlate) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.licensePlate = licensePlate;
+        this.owner = null;
+    }
 
-let car1 = new Car("Honda", "Civic", 2015, "ABC123", human1);
-let car2 = new Car("Toyota", "Corolla", 2018, "DEF456", human2);
-let car3 = new Car("Ford", "Mustang", 2020, "GHI789", human3);
+    assignOwner(owner) {
+        if (owner instanceof Person && owner.age > 18) {
+            this.owner = owner;
+        } else {
+            console.log("Власник має бути особою старше 18 років");
+        }
+    }
 
-car1.printInfo();
-car2.printInfo();
-car3.printInfo();
+    displayInfo() {
+        console.log(`Марка: ${this.brand}, Модель: ${this.model}, Рік: ${this.year}, Номерний знак: ${this.licensePlate}`);
+        if (this.owner) {
+            console.log("Власник:");
+            this.owner.displayInfo();
+            }  else {
+                console.log("Цей автомобіль ще не має власника");
+        };
+    };
+};
+
+// Створення декількох екземплярів класу Людина
+const person1 = new Person("Діма", 30);
+const person2 = new Person("Василь", 31);
+const person3 = new Person("Іван", 15);
+
+// Створення декількох екземплярів класу Автомобіль
+const car1 = new Car("Toyota", "Camry", 2015, "BC1234AA");
+const car2 = new Car("Honda", "Accord", 2018, "AB5678AB");
+const car3 = new Car("Nissan", "Altima", 2020, "BC9999AA");
+
+// Присвоєння власників автомобілям
+car1.assignOwner(person1);
+car2.assignOwner(person2);
+car3.assignOwner(person3);
+
+// Вивід інформації про автомобілі та їх власників
+car1.displayInfo();
+car2.displayInfo();
+car3.displayInfo();
